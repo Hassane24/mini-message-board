e03991420d1a19785a844df4f41d969eda4d6e7f
Hassane <hsisane5@gmail.com>
2023-12-27 22:21:33 +0100

    can submit messages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,45 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Hassane",
+    added: new Date(),
+  },
+  {
+    text: "Hey!!",
+    user: "Aymen",
+    added: new Date(),
+  },
+  {
+    text: "How are you cutie?",
+    user: "Hassane",
+    added: new Date(),
+  },
+  {
+    text: "I'm fine ahbb",
+    user: "Aymen",
+    added: new Date(),
+  },
+];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Message Board", comments: messages });
+});
+
+router.get("/new", function (req, res, next) {
+  res.render("form");
+});
+
+router.post("/new", function (req, res, next) {
+  console.log(req.body);
+  messages.push({
+    text: req.body.message,
+    user: req.body.user,
+    added: new Date(),
+  });
+  res.redirect("/");
 });
 
 module.exports = router;
